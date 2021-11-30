0001086546db8369804f7df5f54d66382635943f
progmafe <fernandaalien13@gmail.com>
2021-11-30 20:10:25 -0300

    Terceiro Commit

---
+++ djs.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dj's</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"/>
+    <link rel="stylesheet" href="./css/djs.css"/>     
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>  
+
+    <header>  
+        <nav class="navbar">
+            <div class="container" id="navbar-links">
+                <a class="navbar-brand"><img id="logo" src="./image/logocolor.png" alt="HighParty"/></a>
+                <a class="nav-item nav-link" href="home.php"><i>Home</i></span></a>
+                <a class="nav-item nav-link" href="hi_mix.php"><i>Hi_mix</i></a>
+                <a class="nav-item nav-link" href="djs.php"><i>Djs</i></a>
+                <a class="nav-item nav-link" href="highparty.php"><i>HighParty<fonte>Rave</fonte></i></a>  
+                <a class="nav-item nav-link" href="presskits.php"><i>Presskits</i></a>    
+            </div>
+        </nav>     
+    </header>
+
+    <section class="paginadjs">
+        <div class="container">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active text-center">
+                        <a href="kodux.php"><img src="./image/DJ1.png" class="imgDj" alt="Kodux"></a>
+                    </div>
+                    <div class="carousel-item text-center">
+                        <a href="azzura.php"><img src="./image/azzura2.png" class="imgDj" alt="Azzura"></a>
+                    </div>
+                    <div class="carousel-item text-center">
+                        <a href="psychocat.php"><img src="./image/PC2.png" class="imgDj" alt="Psychocat"></a>
+                    </div>
+                    <div class="carousel-item text-center">
+                        <a href="ozbits.php"><img src="./image/DJ6.png" class="imgDj" alt="Ozbits"></a>
+                    </div>
+                    <div class="carousel-item text-center">
+                        <a href="chemical.php"><img src="./image/DJ3.png" class="imgDj" alt="ChemicalNoise"></a>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </section>                        
+
+    <div class="footer">Developed by TechRabbit</div>
+
+    <script src="js/scripts.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
